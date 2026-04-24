d01e2c315ddf394a75acbaf42d1b7cc94b3fead1
Fariz Muradov <aze2201@gmail.com>
2026-04-24 17:08:59 +0200

    Delete k8s/nifi-auth/start-nifi.sh

--- k8s/nifi-auth/start-nifi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Rendering nifi.properties from template..."
-
-envsubst < /opt/nifi/conf/nifi.properties.tpl > /opt/nifi/nifi-current/conf/nifi.properties
-
-echo "Starting NiFi..."
-exec /opt/nifi/nifi-current/bin/nifi.sh run
